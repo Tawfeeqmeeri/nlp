050ae8b2edf1d732fc4c62fa758c281b14045ec3
Tawfeeq <Tawfeeq@MacBook.local>
2019-04-24 14:58:55 +0200

    third commit

--- bag_of_poe.py
+++ bag_of_poe.py
@@ -1,4 +1,4 @@
-
+#%%
 import string
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
@@ -13,21 +13,19 @@ file.close()
 
 
 # split file 
-words = text.split()
-words = [word.lower() for word in words]
+split_text = text.split('*')
+
+document = [work for work in split_text]
+
+print(document[:10])
+
+# #remove punctuation 
+# no_punctuation_list = [w.translate(str.maketrans('','', string.punctuation)) for w in words]
+
+# # #remove stop words 
+# stop_words = stopwords.words('english')
+# no_stop_words = [word for word in no_punctuation_list if word not in stop_words]
 
 
-#remove punctuation 
-no_punctuation_list = [w.translate(str.maketrans('','', string.punctuation)) for w in words]
-
-#remove stop words 
-stop_words = stopwords.words('english')
-no_stop_words = [word for word in no_punctuation_list if word not in stop_words]
-
-
-#remove non-alphabetic characters
-clean_words = [word for word in no_stop_words if word.isalpha()]
-print(clean_words[:10])
-
-#bag of words 
-def bag_of_words(clean_words):
+# # #remove non-alphabetic characters
+# clean_words = [word for word in no_stop_words if word.isalpha()]
